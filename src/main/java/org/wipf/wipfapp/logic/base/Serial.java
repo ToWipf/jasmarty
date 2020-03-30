26e0e562640dca9a01b76f4be3fe69202bfcc451
wipf <tobf@web.de>
2020-03-30 20:01:13 +0200

    up

---
+++ src/main/java/org/wipf/wipfapp/logic/base/Serial.java
@@ -0,0 +1,43 @@
+package org.wipf.wipfapp.logic.base;
+
+import java.io.IOException;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+/**
+ * Simple application that is part of an tutorial. The tutorial shows how to
+ * establish a serial connection between a Java and Arduino program.
+ * 
+ * 
+ * http://www.mschoeffler.de/2017/12/29/tutorial-serial-connection-between-java-application-and-arduino-uno/
+ *
+ */
+public class Serial {
+	public static void test() throws IOException, InterruptedException {
+		SerialPort sp = SerialPort.getCommPort("/dev/ttyACM1"); // device name TODO: must be changed
+		sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
+		sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
+
+		if (sp.openPort()) {
+			System.out.println("Port is open :)");
+		} else {
+			System.out.println("Failed to open port :(");
+			return;
+		}
+
+		for (Integer i = 0; i < 5; ++i) {
+			sp.getOutputStream().write(i.byteValue());
+			sp.getOutputStream().flush();
+			System.out.println("Sent number: " + i);
+			Thread.sleep(1000);
+		}
+
+		if (sp.closePort()) {
+			System.out.println("Port is closed :)");
+		} else {
+			System.out.println("Failed to close port :(");
+			return;
+		}
+
+	}
+}
