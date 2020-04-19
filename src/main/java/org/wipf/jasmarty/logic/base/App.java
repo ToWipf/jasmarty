ac4d06711855ad364c1fbe12e2d76db65a163ea1
wipf <tobf@web.de>
2020-04-19 20:30:00 +0200

    new nav

--- src/main/java/org/wipf/jasmarty/logic/base/App.java
+++ src/main/java/org/wipf/jasmarty/logic/base/App.java
@@ -35,7 +35,7 @@ public class App {
 	SerialConfig serialConfig;
 
 	private static final Logger LOGGER = Logger.getLogger("app");
-	public static final String VERSION = "0.201";
+	public static final String VERSION = "0.204";
 	public static final String DB_PATH = "jasmarty.db";
 
 	/**
