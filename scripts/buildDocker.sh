b7ae818e5dcef22f172cb9d6133e5ae01bec0781
wipf <tobf@web.de>
2020-07-25 22:02:29 +0200

    docker

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cp ../target/jasmarty-1.0-SNAPSHOT-runner.jar jasmarty.jar
 docker build -t jasmarty .
-rm jasmarty.jar
+
 
 docker tag jasmarty docker.pkg.github.com/towipf/jasmarty/jasmarty:0.75
 docker push docker.pkg.github.com/towipf/jasmarty/jasmarty:0.75
 
 
 #--platform linux/arm/v7 
+
+docker buildx build --platform linux/arm -t docker.pkg.github.com/towipf/jasmarty/jasmarty:0.75arm -f Dockerfile.arm .
+docker push docker.pkg.github.com/towipf/jasmarty/jasmarty:0.75arm
+
+rm jasmarty.jar
