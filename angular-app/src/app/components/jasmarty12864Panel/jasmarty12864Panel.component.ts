88e9984e9a33f13710ffc817ff3bbf4b779adbae
ToWipf <TobF@web.de>
2020-12-31 01:21:41 +0100

    up

--- angular-app/src/app/components/jasmarty12864Panel/jasmarty12864Panel.component.ts
+++ angular-app/src/app/components/jasmarty12864Panel/jasmarty12864Panel.component.ts
@@ -27,4 +27,45 @@ export class Jasmarty12864PanelComponent implements OnInit {
     this.screen[x][y] = !this.screen[x][y];
   }
 
+  public genScreenToSend() {
+    let jarray: number[] = [];
+
+    for (let x = 0; x < 64; x++) {
+      for (let y = 0; y < (128 / 8); y++) {
+
+        let nr = 0;
+
+        for (let n = 0; n < 8; n++) {
+          console.log(x, y + n);
+          if (this.screen[x][y + n]) {
+            nr = nr + n;
+          }
+        }
+
+        //this.screen[x][y * 8];
+
+        //var ns = parseInt( , 2);
+      }
+    }
+
+
+    // for (let r = 0; r < 1024; r++) {
+    //   let nNr: number = 0;
+    //   for (let nTmp = 0; nTmp < 8; nTmp++) {
+    //     console.log(Math.floor(r / 16), nTmp * r);
+    //     if (this.screen[Math.floor(r / 16)][nTmp]) {
+    //       nNr = nNr + nTmp + 1;
+    //     }
+    //   }
+    //   jarray.push(nNr);
+    // }
+    console.log(jarray);
+  }
+
+  private dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+  }
+
+
+
 }
