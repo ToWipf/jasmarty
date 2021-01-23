f1e5941902be0c32f659bf6169fcc6a846381d53
wipf <tobf@web.de>
2021-01-23 23:09:44 +0100

    up

--- src/main/java/org/wipf/jasmarty/logic/jasmarty/lcd12864/Lcd12864PageConverter.java
+++ src/main/java/org/wipf/jasmarty/logic/jasmarty/lcd12864/Lcd12864PageConverter.java
@@ -10,12 +10,19 @@ import org.wipf.jasmarty.datatypes.jasmarty.Lcd12864Font.Lcd12864fontType;
 import org.wipf.jasmarty.datatypes.jasmarty.Lcd12864Page;
 import org.wipf.jasmarty.datatypes.jasmarty.Lcd12864Page.lineAlignment;
 import org.wipf.jasmarty.datatypes.jasmarty.Lcd12864PageDescription;
+import org.wipf.jasmarty.logic.base.MainHome;
+import org.wipf.jasmarty.logic.base.Wipf;
+import org.wipf.jasmarty.logic.jasmarty.extensions.Winamp;
 
 @ApplicationScoped
 public class Lcd12864PageConverter {
 
 	@Inject
 	Lcd12864Cache cache;
+	@Inject
+	Wipf wipf;
+	@Inject
+	Winamp winamp;
 
 	private Lcd12864PageDescription pd;
 
@@ -31,23 +38,28 @@ public class Lcd12864PageConverter {
 	 */
 	public void refreshCurrentPage() {
 		if (pd != null) {
-			System.out.println("re");
 			cache.setScreen(buildPage(pd));
 		}
 	}
 
 	/**
-	 * curl -d '{"id":1,"name":"testseite1","static":[],"dynamic":[{"type":"TEXT",
-	 * "font":"FONT_57", "data":"11112222", "x":0,"y":0}]}' -X POST
-	 * localhost:8080/lcd12864/savePage
+	 * curl -d
+	 * '{"id":1,"name":"testseite1","static":[],"dynamic":[{"type":"TEXT","font":"FONT_57",
+	 * "data":"11112222", "x":0,"y":0}]}' -X POST localhost:8080/lcd12864/savePage
 	 * 
+	 * 
+	 * curl -d
+	 * '{"id":1,"name":"testseite1","static":[],"dynamic":[{"type":"TEXT","font":"FONT_57",
+	 * "data":"$time(dd MMMM yyyy)", "x":0,"y":0},{"type":"TEXT","font":"FONT_57",
+	 * "data":"$time(dd MMMM yyyy HH-mm-ss)", "x":0,"y":20}]}' -X POST
+	 * localhost:8080/lcd12864/savePage
 	 * 
 	 * @param page
 	 */
 	private Lcd12864Page buildPage(Lcd12864PageDescription pd) {
 		Lcd12864Page lp = new Lcd12864Page();
 		JSONArray jDynamic = pd.getDynamic();
-		JSONArray jStatic = pd.getStatic(); // TODO
+		JSONArray jStatic = pd.getStatic(); // TODO statische inhalte
 
 		for (Object o : jDynamic) {
 			JSONObject jo = new JSONObject(o.toString());
@@ -57,11 +69,11 @@ public class Lcd12864PageConverter {
 				switch (jo.get("font").toString()) {
 				case "FONT_57":
 					lp.drawString(new Lcd12864Font(Lcd12864fontType.FONT_57), jo.getInt("x"), jo.getInt("y"),
-							lineAlignment.CUSTOM, jo.get("data").toString());
+							lineAlignment.CUSTOM, searchAndReplaceVarsInString(jo.get("data").toString()));
 					break;
 				case "FONT_68":
 					lp.drawString(new Lcd12864Font(Lcd12864fontType.FONT_68), jo.getInt("x"), jo.getInt("y"),
-							lineAlignment.CUSTOM, jo.get("data").toString());
+							lineAlignment.CUSTOM, searchAndReplaceVarsInString(jo.get("data").toString()));
 					break;
 				default:
 					break;
@@ -76,4 +88,74 @@ public class Lcd12864PageConverter {
 		return lp;
 	}
 
+	/**
+	 * Alle Variablen finden und und ersetzen
+	 * 
+	 * @param sLine
+	 * @return
+	 * @throws Exception
+	 */
+	private String searchAndReplaceVarsInString(String sLine) {
+		String sOut = sLine;
+
+		int lastIndex = 0;
+		while (lastIndex != -1) {
+			lastIndex = sLine.indexOf("$", lastIndex);
+
+			if (lastIndex != -1) {
+				lastIndex += 1;
+				sOut = lineReplaceVars(sOut);
+			}
+		}
+		return sOut;
+	}
+
+	/**
+	 * @param sLine
+	 * @return
+	 */
+	private String lineReplaceVars(String sLine) {
+		if (sLine.length() < 3) {
+			return sLine;
+		}
+
+		// Positionen bestimmen
+		Integer nIndexStart = sLine.lastIndexOf('$');
+		if (nIndexStart == -1) {
+			return sLine;
+		}
+		Integer nIndexParaStart = sLine.lastIndexOf('(');
+		if (nIndexParaStart == -1) {
+			return sLine;
+		}
+		Integer nIndexEnd = sLine.indexOf(')', nIndexParaStart);
+		if (nIndexEnd == -1) {
+			return sLine;
+		}
+
+		String sBefore = sLine.substring(0, nIndexStart);
+		String sCommand = sLine.substring(nIndexStart + 1, nIndexParaStart);
+		String sParameter = sLine.substring(nIndexParaStart + 1, nIndexEnd);
+		String sAfter = sLine.substring(nIndexEnd + 1, sLine.length());
+
+		// Werte ermittelen
+		switch (sCommand) {
+		case "time":
+			return sBefore + wipf.time(sParameter) + sAfter;
+		case "math":
+			return sBefore + wipf.doMathByString(sParameter) + sAfter;
+		case "rnd":
+			return sBefore + wipf.getRandomInt(sParameter) + sAfter;
+		case "winamp":
+			try {
+				return sBefore + winamp.getInfos(sParameter) + sAfter;
+			} catch (Exception e) {
+				return "Winamp Fail";
+			}
+		case "ver":
+			return sBefore + MainHome.VERSION + sAfter;
+		default:
+			return "Nicht gefunden: " + sCommand;
+		}
+	}
 }
