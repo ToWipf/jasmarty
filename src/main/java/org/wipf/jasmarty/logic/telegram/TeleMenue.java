3d51128acc45d5e750aa9922a9a21afabadc75ef
ToWipf <TobF@web.de>
2021-09-15 19:24:47 +0200

    up

--- src/main/java/org/wipf/jasmarty/logic/telegram/TeleMenue.java
+++ src/main/java/org/wipf/jasmarty/logic/telegram/TeleMenue.java
@@ -92,6 +92,7 @@ public class TeleMenue {
 					"pp \n" +
 					"pm / mp\n" +
 					"setpunkte / sp / ps / mp\n" +
+					"changepunkte / pc / pa N\n" +
 					"itext TEXT IN DER APP\n" +
 					"kill\n" +
 					"system\n" +
@@ -163,6 +164,7 @@ public class TeleMenue {
 			case "pluspunkte":
 				punkteVW.pluspunkt();
 				return punkteVW.getPunkte().toString();
+			case "pa":
 			case "pc":
 			case "punkteChange":
 				punkteVW.appendPunkt(t.getMessageIntPart(1));
