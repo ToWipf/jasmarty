c70f525653d85e17480e1de9e569c80acbc6ca8d
ToWipf <TobF@web.de>
2021-10-30 01:39:14 +0200

    dockerimage_arm

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 cp ../target/jasmarty-1.0-SNAPSHOT-runner.jar jasmarty.jar
 
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+
 #docker build -t jasmarty .
 #docker tag jasmarty docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80
 #docker push docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80
 
 # platform linux/arm/v7 
 
-docker buildx build --platform linux/arm -t docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80-1arm -f Dockerfile.arm .
-docker push docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80-1arm 
+docker buildx build --platform linux/arm -t jatest -f Dockerfile.arm .
+#docker buildx build --platform linux/arm -t docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80-1arm -f Dockerfile.arm .
+#docker push docker.pkg.github.com/towipf/jasmarty/jasmarty:0.80-1arm 
 
 rm jasmarty.jar
