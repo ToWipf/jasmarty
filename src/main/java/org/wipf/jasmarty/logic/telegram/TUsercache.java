14b9e94a4c668b5bbdc05a62a6fe1baaf659fa67
ToWipf <TobF@web.de>
2022-02-25 20:36:02 +0100

    up

--- src/main/java/org/wipf/jasmarty/logic/telegram/TUsercache.java
+++ src/main/java/org/wipf/jasmarty/logic/telegram/TUsercache.java
@@ -228,17 +228,19 @@ public class TUsercache {
 	 */
 	public String getAllAsText() {
 		StringBuilder sb = new StringBuilder();
+		Integer n = 0;
 		try {
 			for (Usercache uc : getAll()) {
 				if (sb.length() > 1) {
 					sb.append("\n\n");
 				}
 				sb.append(uc.toString());
+				n++;
 			}
 		} catch (SQLException e) {
 			sb.append("Fehler 012 " + e);
 		}
-		return sb.toString();
+		return sb.toString() + " Anzahl: " + n;
 	}
 
 }
