90cf4d683c50ce5695369f4ae8b993881fc23bf5
ToWipf <TobF@web.de>
2022-06-16 18:59:54 +0200

    login

--- angular-app/src/app/service/serviceRest.ts
+++ angular-app/src/app/service/serviceRest.ts
@@ -1,10 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Telegram } from '../datatypes';
+import { DialogWartenComponent } from '../dialog/main.dialog';
+import { ServiceWipf } from './serviceWipf';
 
 @Injectable({
   providedIn: 'root',
 })
 
-export class ServiceRest  {
+export class ServiceRest {
+  constructor(private http: HttpClient, public dialog: MatDialog, private rest: ServiceRest, public serviceWipf: ServiceWipf) { }
+
   private host: string = '';
   //private host: string = 'http://localhost:8080/';
 
@@ -20,6 +28,23 @@ export class ServiceRest  {
     let sHref = window.location.href;
     let sTmp = sHref.substring(0, sHref.lastIndexOf('/'));
     this.host = sTmp.substring(0, sTmp.lastIndexOf('/') + 1);
+  }
+
+  public doHttp(path: string): any {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic my-auth-token'
+      })
+    };
+
+
+    const warten = this.dialog.open(DialogWartenComponent, {});
+    this.http.get(this.rest.gethost() + path, httpOptions).subscribe((resdata: any) => {
+      warten.close();
+      return resdata;
+    });
   }
 
 }
