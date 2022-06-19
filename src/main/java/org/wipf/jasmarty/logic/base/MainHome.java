2902f2fd74af648e2302d1ed0e919e0c2bbd7e1f
ToWipf <TobF@web.de>
2022-06-19 18:45:22 +0200

    buttons

--- src/main/java/org/wipf/jasmarty/logic/base/MainHome.java
+++ src/main/java/org/wipf/jasmarty/logic/base/MainHome.java
@@ -42,7 +42,7 @@ public class MainHome {
 	SqlLitePatcher sqlLitePatcher;
 
 	private static final Logger LOGGER = Logger.getLogger("_MainHome_");
-	public static final String VERSION = "1.2.04";
+	public static final String VERSION = "1.2.05";
 	public static final String DB_PATH = "jasmarty.db";
 
 	/**
