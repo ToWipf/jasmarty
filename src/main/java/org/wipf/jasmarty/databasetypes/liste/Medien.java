0bf06f4e25375008c3ec231523582b4e1f7519c3
ToWipf <tobf@web.de>
2023-04-09 18:20:10 +0200

    up

--- src/main/java/org/wipf/jasmarty/databasetypes/liste/Medien.java
+++ src/main/java/org/wipf/jasmarty/databasetypes/liste/Medien.java
@@ -2,7 +2,6 @@ package org.wipf.jasmarty.databasetypes.liste;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/org/wipf/jasmarty/logic/telegram/TeleLogService.java
+++ src/main/java/org/wipf/jasmarty/logic/telegram/TeleLogService.java
@@ -77,7 +77,6 @@ public class TeleLogService {
 				break;
 			}
 		});
-
 	}
 
 }
