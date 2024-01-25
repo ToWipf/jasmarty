6b19f20442c78b76a9b8ec01555ae8e1bc0323e2
ToWipf <TobF@web.de>
2024-01-25 20:37:41 +0100

    del

--- web/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import aaa from "./aaa.svg";
-import bbb from "./bbb.svg";
-
-export {
-    aaa,
-    bbb
-};
--- web/src/components/Fff.jsx
+++
@@ -1,9 +0,0 @@
-const Fff = () => {
-  return (
-    <section className="relative font-poppins py-10 sm:py-16">
-      test
-    </section>
-  );
-};
-
-export default Fff;
