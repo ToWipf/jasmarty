fb408200f91801d5be7f1dfe55ff09fd18163f2d
ToWipf <TobF@web.de>
2024-09-16 21:49:07 +0200

    https://github.com/ToWipf/jasmarty/security/code-scanning/48

--- src/main/java/org/wipf/jasmarty/logic/telegram/TAppOthers.java
+++ src/main/java/org/wipf/jasmarty/logic/telegram/TAppOthers.java
@@ -4,6 +4,7 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 import org.jboss.logging.Logger;
 import org.wipf.jasmarty.logic.base.Wipf;
@@ -96,10 +97,18 @@ public class TAppOthers {
 			String sWitz;
 			sWitz = wipf.httpRequest(Wipf.httpRequestType.GET, "https://funny4you.at/webmasterprogramm/zufallswitz.js.php");
 			String s = sWitz.substring(41, sWitz.length() - 3);
-			String x = URLEncoder.encode(s, "UTF-8").replaceAll("\\<.*?>", "").replaceAll("\\+", " ").replaceAll("%22", "\n").replaceAll("%28", "(").replaceAll("%29", ")").replaceAll("%21", "!").replaceAll("%3F", "? ").replaceAll("%3A", ": ").replaceAll("%2C", ",").replaceAll("%2E", ". ").replaceAll("%E2%80%93", ",").replaceAll("%C3%A2%E2%82%AC%CB%9C", "").replaceAll("%26%238242%3B%21", "\"").replaceAll("%26quot%3B", "").replaceAll("%26%2339%3B", "").replaceAll("%26%238242%3B", "").replaceAll("%C3%A2%E2%82%AC%C5%BE", "").replaceAll("%C3%83%E2%80%9E", "Ä").replaceAll("%26Auml%3B", "Ä").replaceAll("%C3%A4", "ä").replaceAll("%C3%83%C2%A4", "ä").replaceAll("%26auml%3B", "ä").replaceAll("%C3%83%E2%80%93", "Ö").replaceAll("H%C3%83%C2%B6", "Ö").replaceAll("%C3%83%C2%B6", "ö").replaceAll("%26ouml%3B", "ö").replaceAll("%C3%B6", "ö").replaceAll("%26Uuml%3B", "Ü").replaceAll("%26uuml%3B", "ü")
-					.replaceAll("%C3%BC", "ü").replaceAll("%C3%83%C2%BC", "ü").replaceAll("%C3%83%C5%B8", "ß").replaceAll("%26szlig%3B", "ß").replaceAll("%C3%9F", "ß");
 
-			// TODO
+			if (s.length() > 5000) {
+				return ("Witz zu lang");
+			}
+
+			if (Pattern.matches("^(\\+|-)?(\\d+|(\\d*\\.\\d*))?(E|e)?([-+])?(\\d+)?$", s)) {
+				return ("Witz regex fail");
+			}
+
+			String x = URLEncoder.encode(s, "UTF-8").replaceAll("\\<.*?>", "").replaceAll("\\+", " ").replaceAll("%22", "\n").replaceAll("%28", "(").replaceAll("%29", ")").replaceAll("%21", "!").replaceAll("%3F", "? ").replaceAll("%3A", ": ").replaceAll("%2C", ",").replaceAll("%2E", ". ").replaceAll("%E2%80%93", ",").replaceAll("%C3%A2%E2%82%AC%CB%9C", "").replaceAll("%26%238242%3B%21", "\"").replaceAll("%26quot%3B", "").replaceAll("%26%2339%3B", "").replaceAll("%26%238242%3B", "").replaceAll("%C3%A2%E2%82%AC%C5%BE", "").replaceAll("%C3%83%E2%80%9E", "Ä").replaceAll("%26Auml%3B", "Ä").replaceAll("%C3%A4", "ä").replaceAll("%C3%83%C2%A4", "ä").replaceAll("%26auml%3B", "ä").replaceAll("%C3%83%E2%80%93", "Ö").replaceAll("H%C3%83%C2%B6", "Ö").replaceAll("%C3%83%C2%B6", "ö").replaceAll("%26ouml%3B", "ö").replaceAll("%C3%B6", "ö").replaceAll("%26Uuml%3B", "Ü").replaceAll("%26uuml%3B", "ü").replaceAll("%3Chr", " ")
+					.replaceAll("%3Cbr %2F%3E", " ").replaceAll("%2F%3E%3Csmall%3E", "").replaceAll("%C3%BC", "ü").replaceAll("%C3%83%C2%BC", "ü").replaceAll("%C3%83%C5%B8", "ß").replaceAll("%26szlig%3B", "ß").replaceAll("%C3%9F", "ß");
+
 			return x.substring(0, x.indexOf("Ein Witz von"));
 
 		} catch (Exception e) {
