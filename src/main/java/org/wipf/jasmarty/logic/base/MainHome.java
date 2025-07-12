f71898374e223248f5abcf6d10855e259a4866f0
ToWipf <TobF@web.de>
2025-07-12 09:53:14 +0200

    updates

--- src/main/java/org/wipf/jasmarty/logic/base/MainHome.java
+++ src/main/java/org/wipf/jasmarty/logic/base/MainHome.java
@@ -41,7 +41,7 @@ public class MainHome {
 	AuthKeyService authKeyService;
 
 	private static final Logger LOGGER = Logger.getLogger("_MainHome_");
-	public static final String VERSION = "2.4.69";
+	public static final String VERSION = "2.4.70";
 	public static final String AUTH_KEY_NAME = "authKey";
 
 	/**
